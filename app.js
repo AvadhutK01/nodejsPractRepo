b38cfd4e0e51794fddffe4184844cd492156114a
AvadhutK01 <kelaskaravadhut052@gmail.com>
2023-09-29 16:13:32 +0530

    add to cart and delete

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 const app = express();
-
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -16,6 +15,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -39,10 +40,12 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 sequelize.sync().then(result => {
     return User.findByPk(1)
-
-
 }).then(user => {
     if (!user) {
         return User.create({ name: "Avadhut", email: "ak@mail.com" });
@@ -50,8 +53,8 @@ sequelize.sync().then(result => {
     return user;
 }).then(user => {
     //console.log(user);
-    app.listen(3000);
-}).catch(err => {
+    return user.createCart();
+}).then(app.listen(3000)).catch(err => {
     console.log(err);
 })
 
--- controllers/shop.js
+++ controllers/shop.js
@@ -25,10 +25,16 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getCart = (req, res, next) => {
-  res.render('shop/cart', {
-    path: '/cart',
-    pageTitle: 'Your Cart'
-  });
+  req.user.getCart().then(item => {
+    return item.getProducts().then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
+
+    }).catch(err => console.log(err));
+  })
 };
 
 exports.getOrders = (req, res, next) => {
@@ -55,8 +61,39 @@ exports.getProduct = (req, res, next) => {
 }
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productid;
-  Product.findbyId(prodId, (product) => {
-    cart.addProduct(prodId, product.price);
-  })
-  res.redirect('/cart');
-}
+  let fetchedCart;
+  req.user.getCart().then(cart => {
+    fetchedCart = cart;
+    return cart.getProducts({ where: { id: prodId } });
+  }).then(products => {
+    let product;
+    if (products.length > 0) {
+      product = products[0];
+    }
+    let newQuantity = 1;
+    if (product) {
+      newQuantity = newQuantity + product.cartItem.dataValues.quantity;
+    }
+    return Product.findByPk(prodId).then(product => {
+      return fetchedCart.addProduct(product, { through: { quantity: newQuantity } });
+    });
+  }).then(() => {
+    res.redirect('/cart');
+  }).catch(err => {
+    console.log(err);
+  });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  req.user.getCart().then(cart => {
+    return cart.getProducts({ where: { id: prodId } });
+  }).then((products) => {
+    const product = products[0];
+    return product.cartItem.destroy();
+  }).then(() => {
+    res.redirect('/cart');
+  }).catch(err => {
+    console.log(err);
+  });
+};
